63e4ad0b20dfa4a85bb3e6edcba9688b47edda6f
Igor Byazrov <fbi1978@mail.ru>
2022-12-04 12:09:14 +0300

    Changed

---
+++ src/Miles.java
@@ -0,0 +1,12 @@
+public class Miles {
+    public static void main(String[] args) {
+        int price;
+        int bonusMiles;
+        price = 1000;
+        bonusMiles = 20;
+
+        int bonus = price / bonusMiles;
+
+        System.out.println(" Начисленные бонусные мили " + bonus);
+    }
+}
